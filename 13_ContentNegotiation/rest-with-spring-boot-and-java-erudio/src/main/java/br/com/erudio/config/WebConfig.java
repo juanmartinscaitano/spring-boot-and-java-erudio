6220c8ed1f161a5331818c51d89b726a809c8c79
Juan <juanmartinscaitano@gmail.com>
2025-09-22 09:43:27 -0300

    refactor: added anotation @Congiguration and implements WebMvcCongigurer, add method configureNegotiation to accept Json and Xml by Query Param

--- 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,6 +1,27 @@
 package br.com.erudio.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+
+        //By EXTENSION http://localhost:8080/api/person/v1/2.xml or http://localhost:8080/api/person/v1/2.JSON Deprecated on Spring 2.6
+
+        //By QUERY PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
+        configurer.favorParameter(true)
+            .parameterName("mediaType")
+            .ignoreAcceptHeader(true)
+            .useRegisteredExtensionsOnly(false)
+            .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
 }
 
-//feature
