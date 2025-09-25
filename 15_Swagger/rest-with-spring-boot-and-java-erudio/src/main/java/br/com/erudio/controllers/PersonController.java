8fe968d6abddbbe24d601d6d9f48ed4fd3868177
Juan <juanmartinscaitano@gmail.com>
2025-09-25 08:21:09 -0300

    feature: added anotation @Tag in PeronController.class

--- 15_Swagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 15_Swagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -2,6 +2,7 @@ package br.com.erudio.controllers;
 
 import br.com.erudio.data.dto.PersonDTO;
 import br.com.erudio.services.PersonServices;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "End Points for Managing People")
 public class PersonController {
 
     @Autowired
