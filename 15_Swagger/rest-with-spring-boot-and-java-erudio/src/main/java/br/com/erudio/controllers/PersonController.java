e9defb4f441e6461a8faa35b2d7756e3210179fc
Juan <juanmartinscaitano@gmail.com>
2025-09-25 08:35:51 -0300

    feature: created documentation for Find All

--- 15_Swagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 15_Swagger/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -2,6 +2,11 @@ package br.com.erudio.controllers;
 
 import br.com.erudio.data.dto.PersonDTO;
 import br.com.erudio.services.PersonServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -24,6 +29,25 @@ public class PersonController {
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_YAML_VALUE
     })
+    @Operation(summary = "Find All People",
+        description = "Finds All People",
+        tags = {"People"},
+        responses = {
+            @ApiResponse(description = "Success"
+                , responseCode = "200"
+                , content = {
+                    @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class))
+                    )
+            }),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
